37b772ea289a82ce42a10f109e0f70634c13e981
Guru sai pradeep <gurusaipradeepy@gmail.com>
2025-04-06 05:05:16 +0530

    I have learned how to fetch the data from real API and I styled the interface as well

--- src/Components/NewLetter.jsx
+++ src/Components/NewLetter.jsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewLetter extends Component {
     render() {
+        let { title, description, imageUrl, url } = this.props;
         return (
             <>
-                <p>This is NewsLetter</p>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={imageUrl ? imageUrl : "https://as1.ftcdn.net/v2/jpg/04/60/01/36/1000_F_460013622_6xF8uN6ubMvLx0tAJECBHfKPoNOR5cRa.jpg"} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={url} target='_blank' className="btn btn-sm btn-primary">Read more</a>
+                    </div>
+                </div>
             </>
         )
     }
--- src/Components/NewsComp.jsx
+++ src/Components/NewsComp.jsx
@@ -2,11 +2,32 @@ import React, { Component } from 'react'
 import NewLetter from './NewLetter'
 
 export default class NewsComp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            articles: []
+        }
+    }
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=aee2e184d5ac4e6facb45cc282f6d7af"
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({ articles: parsedData.articles });
+    }
     render() {
         return (
             <>
-                <h1>This is NewsLetter Component</h1>
-                <NewLetter />
+                <div className="container my-3">
+                    <center><h1>Vartha Samachar - Top Headlines</h1></center>
+                    <div className="row my-3">
+                        {this.state.articles.map((element) => {
+                            return <div className="col-md-4 my-3" key={element.url}>
+                                <NewLetter title={element.title ? element.title.slice(0, 20) : ""} description={element.description ? element.description.slice(0, 50) : ""} imageUrl={element.urlToImage} url={element.url} />
+                            </div>
+                        })}
+                    </div>
+                </div>
             </>
         )
     }
