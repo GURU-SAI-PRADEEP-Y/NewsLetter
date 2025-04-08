b0b6c823a0daf2ea61b8b53bc04bb14576793fca
Guru sai pradeep <gurusaipradeepy@gmail.com>
2025-04-09 01:26:39 +0530

    I have added the previous and next buttons as well as their functionalities

--- src/Components/NewsComp.jsx
+++ src/Components/NewsComp.jsx
@@ -5,16 +5,44 @@ export default class NewsComp extends Component {
     constructor() {
         super();
         this.state = {
-            articles: []
+            articles: [],
+            page: 1
         }
     }
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=aee2e184d5ac4e6facb45cc282f6d7af"
+        let url = "https://newsapi.org/v2/everything?q=apple&from=2025-04-04&to=2025-04-04&sortBy=popularity&apiKey=aee2e184d5ac4e6facb45cc282f6d7af&page=1&pageSize=20"
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
         this.setState({ articles: parsedData.articles });
     }
+
+    handlePrevClick = async () => {
+        console.log("Prev");
+        let url = `https://newsapi.org/v2/everything?q=apple&from=2025-04-04&to=2025-04-04&sortBy=popularity&apiKey=aee2e184d5ac4e6facb45cc282f6d7af&page=${this.state.page - 1}&pageSize=20`
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({ articles: parsedData.articles });
+        this.setState({ page: this.state.page - 1 });
+
+
+    }
+    handleNextClick = async () => {
+        console.log("next");
+        if (this.state.page + 1 > Math.ceil(this.state.totalResult / 20)) {
+
+        }
+        else {
+            let url = `https://newsapi.org/v2/everything?q=apple&from=2025-04-04&to=2025-04-04&sortBy=popularity&apiKey=aee2e184d5ac4e6facb45cc282f6d7af&page=${this.state.page + 1}&pageSize=20`
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            console.log(parsedData);
+            this.setState({ articles: parsedData.articles });
+            this.setState({ page: this.state.page + 1 });
+        }
+    }
+
     render() {
         return (
             <>
@@ -26,6 +54,10 @@ export default class NewsComp extends Component {
                                 <NewLetter title={element.title ? element.title.slice(0, 20) : ""} description={element.description ? element.description.slice(0, 50) : ""} imageUrl={element.urlToImage} url={element.url} />
                             </div>
                         })}
+                    </div>
+                    <div className="container d-flex justify-content-between">
+                        <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+                        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                     </div>
                 </div>
             </>
